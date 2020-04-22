8bf86e0cd862d0b62f6f89decfd1ee0c9efbabc0
D_K <email2git@gmail.com>
2020-04-22 15:02:25 +0300

    format getArray

--- src/values/getArray.js
+++ src/values/getArray.js
@@ -1,45 +1,55 @@
 'use strict';
-import ALERTY     from '../alerty/alerty.js';
+import ALERTY from '../alerty/alerty.js';
 
 export default function GetType(newData) {
     if (!(this instanceof GetType)) return new GetType(newData);
-    let errorAlert = function(text,orData){
-        ALERTY.error("Data type error! "+ text +" instead of ARRAY! Now use ", orData);
+    let errorAlert = function (text, orData) {
+        ALERTY.error("Data type error! " + text + " instead of ARRAY! Now use ", orData);
     };
 
     this.newData = newData;
-    this.or = function(orData = []) {
+    this.or = function (orData = []) {
         let returnedArr = this.newData;
-        switch (this.newData){
-            case null : returnedArr = orData; errorAlert("NULL",orData); break;
-            case undefined : returnedArr = orData; errorAlert("undefined",orData); break;
+        switch (this.newData) {
+            case null:
+                returnedArr = orData;
+                errorAlert("NULL", orData);
+                break;
+            case undefined:
+                returnedArr = orData;
+                errorAlert("undefined", orData);
+                break;
         }
-        if(_.isNumber(this.newData)){
-            returnedArr = [this.newData]; errorAlert("Number",orData);
+        if (_.isNumber(this.newData)) {
+            returnedArr = [this.newData];
+            errorAlert("Number", orData);
         }
         return returnedArr;
     }
-    this.type = function(type){
+    this.type = function (type) {
         let returnedArr = this.newData;
-        switch(type){
-            case "INT": returnedArr = parseInt(); break;
+        switch (type) {
+            case "INT":
+                returnedArr = parseInt();
+                break;
         }
         return returnedArr;
     }
-    function parseInt(){
-       let parsed = [];
-       for (var i = 0; i <  this.newData.length; i++) {
-           let el =  this.newData[i];
-            if(Number.isInteger(el)){
+
+    function parseInt() {
+        let parsed = [];
+        for (var i = 0; i < this.newData.length; i++) {
+            let el = this.newData[i];
+            if (Number.isInteger(el)) {
                 parsed.push(el)
-            }else if(typeof el == 'number'){
+            } else if (typeof el == 'number') {
                 parsed.push(parseInt(el))
-            }else if(typeof el == 'string'){
+            } else if (typeof el == 'string') {
                 parsed.push(el - 0)
             } else {
                 parsed.push(null)
             }
-       }
-       return parsed;
+        }
+        return parsed;
     }
 }
