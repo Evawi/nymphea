7c464aec69e4410ccb50a92a0c76252a70f9dce8
D_K <email2git@gmail.com>
2020-05-06 23:27:04 +0300

    mergObjects update
    add  deepCloneObj(), non JSON
    objMerge = (obj1 + obj2), priority obj1
    objDiff = (objMerge - obj2)

--- src/values/mergObjects.js
+++ src/values/mergObjects.js
@@ -9,28 +9,39 @@ export default function MergObjects() {
         return new MergObjects();
     }
 
-    // Result merging - obj2
-    // Return - difference
-    this.mergObj = function(obj1, obj2){
+    this.deepCloneObj = function(obj){
+        let objClone = {};
+        for (let key in obj) {
+            objClone[key] = obj[key]; 
+            if (gt.typeIdent(obj[key]) == 'object'){
+                this.deepCloneObj(obj[key]);
+            }
+        }
+        return objClone;
+    }
+
+    // objMerge = (obj1 + obj2), priority obj1
+    // objDiff = (objMerge - obj2)
+    this.mergObj = function(obj1, obj2) {
+        let objMerge = this.deepCloneObj(obj2);
+        let objDiff = {};
     
-        let difference = {};
-
         for (let key in obj1){
-
+            
             if(!obj2.hasOwnProperty(key)){
-                difference[key] = obj1[key];
+                objDiff[key] = obj1[key];
             }else{
                 if(obj2[key] != obj1[key]){
-                    difference[key] = obj1[key];
+                    objDiff[key] = obj1[key];
                 }
-            }        
-
-            obj2[key] = obj1[key]; 
+            }   
+            
+            objMerge[key] = obj1[key]; 
             
             if (gt.typeIdent(obj1[key]) == 'object'){
-                this.mergObj(obj1[key],obj2[key]);
+                this.mergObj(obj1[key],objMerge[key]);
             } 
         }
-        return difference;
+        return {objDiff, objMerge};
     }
 }
